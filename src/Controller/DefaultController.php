070581b32fe560869974389205c7818d2924690d
Fedor Gromak <fgromak@iseck.com>
2018-03-04 17:34:27 +0300

    Merge branch 'master' of https://github.com/testTtask/cities
    # Conflicts:
    #	config/packages/security.yaml
    #	src/Migrations/Version20180304133521.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Tests\Controller;
 
 class DefaultController extends Controller
 {
@@ -21,7 +20,7 @@ class DefaultController extends Controller
         $number = mt_rand(0, 100);
 
         return new Response(
-            '<html><body>Lucky number: '.$number.'<br><a href="'.$this->get('router')->generate('app_lucky_number').'"><button>Выход</button></a></body></html>'
+            '<html><body>Lucky number: '.$number.'<br><a href="'.$this->get('router')->generate('logout').'"><button>Выход</button></a></body></html>'
         );
     }
 
