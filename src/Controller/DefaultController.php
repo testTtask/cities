0d7352e5d70983198c65a18b46c045d18a3db098
Andrey <masteri666666@gmail.com>
2018-03-04 11:37:08 +0300

    symfony install

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function index()
+    {
+        return new Response(
+            '<html><body>Success</body></html>'
+        );
+    }
+
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+}
