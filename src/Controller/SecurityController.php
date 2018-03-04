737b3b69d471f8587286b3894143692b18b64ee3
Andrey <masteri666666@gmail.com>
2018-03-04 17:42:48 +0300

    Merge branch 'master' of https://github.com/testTtask/cities
    # Conflicts:
    #	config/packages/security.yaml
    #	src/Entity/User.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,24 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
-    public function login(Request $request, AuthenticationUtils $authUtils)
+    public function login(AuthenticationUtils $authUtils)
     {
-        // получить ошибку входа, если она есть
         $error = $authUtils->getLastAuthenticationError();
-
-        // последнее имя пользователя, введенное пользователем
         $lastUsername = $authUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));
+        $defaults = array(
+            '_target_path' => $this->get('router')->generate('app_lucky_number')
+        );
 
+        $form = $this->createFormBuilder($defaults, [
+            'action' => $this->get('router')->generate('login'),
+            'method' => 'POST',
+        ])
+            ->add('_username', TextType::class)
+            ->add('_password', PasswordType::class)
+            ->add('_target_path', HiddenType::class)
+            ->getForm();
+
+        return $this->render('security/login.html.twig', [
+            'form' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 
 }
